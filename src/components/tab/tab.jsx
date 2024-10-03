13d00a69779681b916213d20fc37f336a6b06aa3
asil1122 <asilbekabdurashidov000gmail.com>
2024-10-03 14:58:46 +0500

    finish

--- src/components/tab/tab.jsx
+++ src/components/tab/tab.jsx
@@ -7,16 +7,16 @@ import { data3 } from "../../data/data3";
 export const Tab = () => {
     const [count, setCount] = React.useState(1);
     const content =
-      count == 1 ? data1 : count == 2 ? data2 : count == 3 ? data3 : [];
-    
+        count == 1 ? data1 : count == 2 ? data2 : count == 3 ? data3 : [];
+
     return (
         <section className={style.tab}>
             <div className="container">
                 <div className={style.block}>
                     <div className={style.btns}>
-                        <button onClick={()=> setCount(1)}>Хиты</button>
-                        <button onClick={()=> setCount(2)}>Скидки</button>
-                        <button onClick={()=> setCount(3)}>Новинки</button>
+                        <button className={style.hit} style={{backgroundColor:count == 1 ? "white" : "#eff2f4"}} onClick={() => setCount(1)}>Хиты</button>
+                        <button className={style.skidka} style={{backgroundColor:count == 2 ? "white" : "#eff2f4"}} onClick={() => setCount(2)}>Скидки</button>
+                        <button className={style.newItem} style={{backgroundColor:count == 3 ? "white" : "#eff2f4"}} onClick={() => setCount(3)}>Новинки</button>
                     </div>
                     <div className={style.item_block}>
                         {content.map((item) => (
